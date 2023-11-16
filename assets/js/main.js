753767de197e263c9c3df22c98982fed0dcf9fc6
Kanade <126738857+ReinKageno@users.noreply.github.com>
2023-11-16 10:17:27 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const jsButton = document.getElementById('journalButton');
+
+jsButton.addEventListener('touchstart', () => {
+    jsButton.style.color = '#0d6efd';
+});
+
+jsButtom.addEventListener('touchend', () => {
+    jsButton.style.color = '#ffffff';
+});
